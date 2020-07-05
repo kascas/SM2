f431a2d16a5b0402959ad582e6deb020a5e131ab
kascas <liangxp2360@163.com>
2020-07-05 15:01:14 +0800

    add Convert.java

---
+++ src/sm2/Curve.java
@@ -0,0 +1,28 @@
+package sm2;
+
+import java.math.BigInteger;
+
+/**
+ * @author: DELL
+ * @date: 2020/7/5-14:55
+ * @description:
+ */
+public class Curve {
+    
+    /**
+     * SM2椭圆曲线公钥密码算法推荐曲线参数
+     */
+    public static final BigInteger n = new BigInteger(
+            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "7203DF6B" + "21C6052B" + "53BBF409" + "39D54123", 16);
+    public static final BigInteger p = new BigInteger(
+            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "00000000" + "FFFFFFFF" + "FFFFFFFF", 16);
+    public static final BigInteger a = new BigInteger(
+            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "00000000" + "FFFFFFFF" + "FFFFFFFC", 16);
+    public static final BigInteger b = new BigInteger(
+            "28E9FA9E" + "9D9F5E34" + "4D5A9E4B" + "CF6509A7" + "F39789F5" + "15AB8F92" + "DDBCBD41" + "4D940E93", 16);
+    public static final BigInteger gx = new BigInteger(
+            "32C4AE2C" + "1F198119" + "5F990446" + "6A39C994" + "8FE30BBF" + "F2660BE1" + "715A4589" + "334C74C7", 16);
+    public static final BigInteger gy = new BigInteger(
+            "BC3736A2" + "F4F6779C" + "59BDCEE3" + "6B692153" + "D0A9877C" + "C62A4740" + "02DF32E5" + "2139F0A0", 16);
+    
+}
--- src/sm2/support/ECPoint.java
+++ src/sm2/support/ECPoint.java
@@ -3,42 +3,17 @@ package sm2.support;
 import java.math.BigInteger;
 import java.util.Random;
 
+import static sm2.Curve.gx;
+import static sm2.Curve.gy;
+import static sm2.Curve.p;
+import static sm2.Curve.a;
+import static sm2.Curve.b;
+import static sm2.Curve.n;
 
 public class ECPoint {
+    
     private BigInteger x;
     private BigInteger y;
-    
-    /**
-     * SM2椭圆曲线公钥密码算法推荐曲线参数
-     */
-    private static BigInteger n = new BigInteger(
-            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "7203DF6B" + "21C6052B" + "53BBF409" + "39D54123", 16);
-    private static BigInteger p = new BigInteger(
-            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "00000000" + "FFFFFFFF" + "FFFFFFFF", 16);
-    private static BigInteger a = new BigInteger(
-            "FFFFFFFE" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "FFFFFFFF" + "00000000" + "FFFFFFFF" + "FFFFFFFC", 16);
-    private static BigInteger b = new BigInteger(
-            "28E9FA9E" + "9D9F5E34" + "4D5A9E4B" + "CF6509A7" + "F39789F5" + "15AB8F92" + "DDBCBD41" + "4D940E93", 16);
-    private static BigInteger gx = new BigInteger(
-            "32C4AE2C" + "1F198119" + "5F990446" + "6A39C994" + "8FE30BBF" + "F2660BE1" + "715A4589" + "334C74C7", 16);
-    private static BigInteger gy = new BigInteger(
-            "BC3736A2" + "F4F6779C" + "59BDCEE3" + "6B692153" + "D0A9877C" + "C62A4740" + "02DF32E5" + "2139F0A0", 16);
-    
-    //小素数测试数据
-    /*
-    private static BigInteger n = new BigInteger(
-            "21", 10);
-    private static BigInteger p = new BigInteger(
-            "19", 10);
-    private static BigInteger a = new BigInteger(
-            "1", 10);
-    private static BigInteger b = new BigInteger(
-            "1", 10);
-    private static BigInteger gx = new BigInteger(
-            "9", 10);
-    private static BigInteger gy = new BigInteger(
-            "6", 10);
-    */
     
     /**
      * 构造非无穷远点
